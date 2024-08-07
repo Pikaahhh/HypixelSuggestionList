f88d3e4315b0626eb862b534a69934f578f21e3b
Pikaahhh <69829232+Pikaahhh@users.noreply.github.com>
2024-08-07 21:49:39 +1000

    Update

--- assets/functions.js
+++ assets/functions.js
@@ -1,55 +1,52 @@
-api = "TEMP";
+var item_list = "";
+var display_list = "";
 
-function search() {
-    input = document.getElementById("username");
-    if (!input.value == "") {
-        sessionStorage.setItem("username", input.value);
-        window.location.href = "search/";
+function addItem() {
+    let item = document.getElementById("items").value;
+    let location = document.getElementById("item_loc").value;
+    let enchant = document.getElementById("enchant");
+    let nbt = document.getElementById("nbt").value;
+
+    if (item == "") {
+        document.getElementById("action").innerHTML = "Missing Item Name!";
+    } else if (location == "") {
+        document.getElementById("action").innerHTML = "Missing Item Location!";
     } else {
-        document.getElementById("error").innerHTML = "No username provided! What the flip..";
-    }
-}
-
-function callUUID() {
-    const r = new XMLHttpRequest(); // r = request
-    r.onload = function() {
-        p = JSON.parse(this.responseText); // p = player
-        sessionStorage.setItem("UUID", p.uuid);
-        console.log(p.uuid);
-        document.getElementById("username").innerHTML = p.username;
-        callProfiles();
-    }
-    r.open("GET", "https://api.ashcon.app/mojang/v2/user/" + sessionStorage.getItem("username"));
-    r.send();
-}
-
-function callProfiles() {
-    const r = new XMLHttpRequest(); // r = request again.
-    r.onload = function() {
-        pr = JSON.parse(this.responseText); // pr = profile.
-        let prList = "";
-        for (i = 0; i < pr.profiles.length; i++) {
-            prList += "<a class='link' href='#"+ pr.profiles[i].cute_name +"' onclick='callProfile("+ i +")'>"+ pr.profiles[i].cute_name +"</a>";
-            if (i < pr.profiles.length - 1) {
-                prList += " | ";
-            }
+        if (enchant.checked == true) {
+            var enchantMe = ",enchant";
+        } else {
+            var enchantMe = "";
         }
-        document.getElementById("profiles").innerHTML = prList;
+        if (nbt == "") {
+            var nbtme = "";
+        } else {
+            var nbtme = "," + nbt.toString();
+        }
+        item_list += item + nbtme + enchantMe + ":" + location + "%%";
+        var ze_item = item + nbtme + enchantMe + ":" + location;
+        console.log(ze_item);
+        display_list += "<span id='" + ze_item + "'>" + ze_item + "</span><br>";
+        document.getElementById("items").value = "";
+        document.getElementById("item_loc").value = "";
+        document.getElementById("action").innerHTML = "Sucessfully added '" + item + ":" + location + "'";
+        document.getElementById("display_list").innerHTML = display_list;
     }
-    r.open("GET", "https://api.hypixel.net/v2/skyblock/profiles?uuid=" + sessionStorage.getItem("UUID") + "&key=" + api);
-    r.send();
 }
 
-function callProfile(ProfileID) {
-    const r = new XMLHttpRequest();
-    r.onload = function() {
-        pr = JSON.parse(this.responseText); // pr = profile again.
-        let p = pr.profiles[ProfileID];
-        console.log(p)
-        uuid = sessionStorage.getItem("UUID").replaceAll("-", "");
-        console.log(p.members);
-        console.log(p.members.uuid);
-    }
-    r.open("GET", "https://api.hypixel.net/v2/skyblock/profiles?uuid=" + sessionStorage.getItem("UUID") + "&key=" + api);
-    r.send();
+function updateCommand() {
+    let output = document.getElementById("output");
+    let container_name = document.getElementById("container_name").value;
+    let size = document.getElementById("rows").value;
+    let view_type = document.getElementById("show_publicly").value;
+
+    output.innerHTML = "/gen2 inventory rows:" + size + " slots_per_row:9 container_name:" + container_name + " draw_border:True show_publicly:" + view_type + " inventory_string:" 
+    + item_list;
 }
+
+function copyCommand() {
+    let output = document.getElementById("output");
+    navigator.clipboard.writeText(output.innerHTML);
+    alert("Copied your command to clipboard!" + output.innerHTML);
+}
+
+setInterval(updateCommand, 500);
